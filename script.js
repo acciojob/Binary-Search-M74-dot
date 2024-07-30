fe28144e86d4e3395702fa55dc6dfcdec3b41961
Manisha Mali <82174253+M74-dot@users.noreply.github.com>
2024-07-30 22:47:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,6 +2,14 @@ function binarySearch(nums, target) {
 // Your code here
 // Return index if target is present
 // else return -1
+	let i=0, j=nums.length-1;
+	while(i<=j){
+		let mid = parseInt((i+j)/2);
+		if(nums[mid]==target)return mid;
+		else if(nums[mid]<target)i=mid+1;
+		else j=mid-1;
+	}
+	return -1;
 }
 
 module.exports = binarySearch;
